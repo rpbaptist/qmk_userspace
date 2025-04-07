c08103784e4a56612d034d9b617bf590afea1af5
Richard Baptist <code@richardbaptist.nl>
2025-04-07 16:10:13 +0200

    update

--- keyboards/capsunlocked/cu7/keymaps/rpbaptist/keymap.c
+++ keyboards/capsunlocked/cu7/keymaps/rpbaptist/keymap.c
@@ -41,52 +41,68 @@ void matrix_scan_user(void) {
 
 #define RPLAY LCTL(LSFT(KC_S))
 #define SGIF LCTL(LSFT(KC_J))
-#define SHOT LCTL(LSFT(KC_I))
 
-#define GAME DF(_GAME)
-#define MEDIA DF(_MEDIA)
-#define UTIL DF(_UTIL)
+#define T_MEDIA TT(_MEDIA)
+#define S_MEDIA MO(_MEDIA)
+
+#define T_UTIL TT(_UTIL)
+#define S_UTIL MO(_UTIL)
 
 #define STEAM LSFT(KC_TAB)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_GAME]  = LAYOUT(RPLAY, GAME, MEDIA, UTIL, SHOT, SGIF, STEAM),
-    [_MEDIA] = LAYOUT(KC_MUTE, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT),
-    [_UTIL]  = LAYOUT(UG_TOGG, QK_BOOT, UG_SATU, UG_VALU, UG_NEXT, UG_SATD, UG_VALD),
+    [_GAME]  = LAYOUT(
+        KC_ENTER,
+        KC_GRV, T_MEDIA, T_UTIL,
+        RPLAY,   SGIF,    STEAM
+    ),
+    [_MEDIA] = LAYOUT(
+        KC_MUTE,
+        _______, S_MEDIA, _______,
+        KC_MPRV, KC_MPLY, KC_MNXT
+    ),
+    [_UTIL]  = LAYOUT(
+        UG_TOGG,
+        QK_BOOT, UG_SATU, S_UTIL,
+        UG_NEXT, UG_SATD, UG_VALD
+    ),
 };
 
 bool encoder_update_user(uint8_t index, bool clockwise) {
-    switch (get_highest_layer(default_layer_state)) {
-        case _MEDIA:
+    switch (get_highest_layer(layer_state)) {
+    case _GAME:
+        if (clockwise) {
+            if (!alt_tab_active) {
+                alt_tab_active = true;
+                unregister_code(KC_LEFT_SHIFT);
+                register_code(KC_LEFT_ALT);
+            }
+            alt_tab_timer = timer_read();
+            tap_code(KC_TAB);
+        } else {
+            if (!alt_shift_tab_active) {
+                alt_shift_tab_active = true;
+                register_code(KC_LEFT_ALT);
+                register_code(KC_LEFT_SHIFT);
+            }
+            alt_tab_timer = timer_read();
+            tap_code(KC_TAB);
+        }
+        break;
+    case _MEDIA:
             if (clockwise) {
                 tap_code_delay(KC_VOLU, 10);
             } else {
                 tap_code_delay(KC_VOLD, 10);
             }
-        case _UTIL:
+            break;
+    case _UTIL:
             if (clockwise) {
                 rgblight_increase_hue();
             } else {
                 rgblight_decrease_hue();
             }
-        case _GAME:
-            if (clockwise) {
-                if (!alt_tab_active) {
-                    alt_tab_active = true;
-                    unregister_code(KC_LEFT_SHIFT);
-                    register_code(KC_LEFT_ALT);
-                }
-                alt_tab_timer = timer_read();
-                tap_code(KC_TAB);
-            } else {
-                if (!alt_shift_tab_active) {
-                    alt_shift_tab_active = true;
-                    register_code(KC_LEFT_ALT);
-                    register_code(KC_LEFT_SHIFT);
-                }
-                alt_tab_timer = timer_read();
-                tap_code(KC_TAB);
-            }
+            break;
     }
     return false;
 }
