92953e0b86c19de9e40ccac1c97c5da5f614f4a6
Richard Baptist <code@richardbaptist.nl>
2025-04-07 16:10:34 +0200

    decrease debounce

--- keyboards/crkbd/keymaps/rpbaptist/config.h
+++ keyboards/crkbd/keymaps/rpbaptist/config.h
@@ -31,7 +31,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define SPLIT_ACTIVITY_ENABLE
 #define SPLIT_LAYER_STATE_ENABLE
 
-#define DEBOUNCE 30
+#define DEBOUNCE 25
 
 #ifdef RGB_MATRIX_ENABLE
 // Undefine r2g effects
