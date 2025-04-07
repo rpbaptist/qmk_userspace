32a09fab0324d19f5f0a01d847bc8041348558b5
Richard Baptist <code@richardbaptist.nl>
2025-04-07 15:36:10 +0200

    Update keymap

--- keyboards/capsunlocked/cu7/keymaps/rpbaptist/keymap.c
+++ keyboards/capsunlocked/cu7/keymaps/rpbaptist/keymap.c
@@ -14,29 +14,79 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT(
-             MO(1),
-    KC_1,    KC_2,    KC_3,
-    KC_4,    KC_5,    KC_6
-  ),
-  [1] = LAYOUT(
-             _______,
-    RGB_MOD, KC_UP,   QK_BOOT,
-    KC_LEFT, KC_DOWN, KC_RGHT
-  ),
+enum layer_names {
+    _GAME,
+    _MEDIA,
+    _UTIL,
 };
 
-// Volume up/down on the encoder
-bool encoder_update_user(uint8_t index, bool clockwise) {
-  if (clockwise) {
-    tap_code_delay(KC_VOLU, 10);
-  }
-  else {
-    tap_code_delay(KC_VOLD, 10);
-  }
+bool     alt_tab_active       = false;
+bool     alt_shift_tab_active = false;
+uint16_t alt_tab_timer        = 0;
 
-  return true;
+// ALT TAB Encoder Timer
+void matrix_scan_user(void) {
+    if (alt_tab_active) {
+        if (timer_elapsed(alt_tab_timer) > 1000) {
+            unregister_code(KC_LEFT_ALT);
+            unregister_code(KC_LEFT_SHIFT);
+            alt_tab_active       = false;
+            alt_shift_tab_active = false;
+        }
+    }
+};
+
+#define RPLAY LCTL(LSFT(KC_S))
+#define SGIF LCTL(LSFT(KC_J))
+#define SHOT LCTL(LSFT(KC_I))
+
+#define GAME DF(_GAME)
+#define MEDIA DF(_MEDIA)
+#define UTIL DF(_UTIL)
+
+#define STEAM LSFT(KC_TAB)
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [_GAME]  = LAYOUT(RPLAY, GAME, MEDIA, UTIL, SHOT, SGIF, STEAM),
+    [_MEDIA] = LAYOUT(KC_MUTE, _______, _______, _______, KC_MPRV, KC_MPLY, KC_MNXT),
+    [_UTIL]  = LAYOUT(UG_TOGG, QK_BOOT, UG_SATU, UG_VALU, UG_NEXT, UG_SATD, UG_VALD),
+};
+
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    switch (get_highest_layer(default_layer_state)) {
+        case _MEDIA:
+            if (clockwise) {
+                tap_code_delay(KC_VOLU, 10);
+            } else {
+                tap_code_delay(KC_VOLD, 10);
+            }
+        case _UTIL:
+            if (clockwise) {
+                rgblight_increase_hue();
+            } else {
+                rgblight_decrease_hue();
+            }
+        case _GAME:
+            if (clockwise) {
+                if (!alt_tab_active) {
+                    alt_tab_active = true;
+                    unregister_code(KC_LEFT_SHIFT);
+                    register_code(KC_LEFT_ALT);
+                }
+                alt_tab_timer = timer_read();
+                tap_code(KC_TAB);
+            } else {
+                if (!alt_shift_tab_active) {
+                    alt_shift_tab_active = true;
+                    register_code(KC_LEFT_ALT);
+                    register_code(KC_LEFT_SHIFT);
+                }
+                alt_tab_timer = timer_read();
+                tap_code(KC_TAB);
+            }
+    }
+    return false;
 }
